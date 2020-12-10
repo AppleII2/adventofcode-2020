201d5467c5d69391633404a74091161fb47b19a2
Nelson Healy <nelson@atom.industries>
2020-12-10 16:48:19 -0500

    day 10 part 2

--- 10.py
+++ 10.py
@@ -1,17 +1,37 @@
+from itertools import combinations
+
 def load(path):
     with open(path, 'r') as raw:
         return [int(x) for x in raw.read().strip().split('\n')]
 
-def part1(data):
+def part1(data, verbose = False):
     a = sorted(list(data) + [0] + [max(data) + 3])
     diffs = []
     for i in range(1, len(a)):
         diffs.append(a[i] - a[i-1])
-    print(diffs.count(1) * diffs.count(3))
+    if verbose: 
+        print(diffs.count(1) * diffs.count(3))
     return diffs
 
-td = (16,10,15,5,1,11,7,19,6,12,4)
-td1 = (1,4,5,6,7,8,11,14,15,16,17,20,23,24,25,28)
+def check_perm(p):
+    if set(part1(p)).difference(set([1,2,3])):
+        return False
+    else:
+        return True
+
+def calc_perms(n):
+    x = [1] + list(range(4, 4+n+1))
+    x += [max(x) + 3]
+    x = tuple(x)
+    ncr = []
+    for i in range(1,len(x)+1):
+        ncr += list(combinations(x,i))
+    ncr = [a for a in ncr if a[0] == 1 and a[-1] == max(x)]
+    valid_ncr = []
+    for comb in ncr:
+        if check_perm(comb):
+            valid_ncr.append(comb)
+    return len(valid_ncr)
 
 def part2(data):
     diffs = part1(data)
@@ -20,7 +40,7 @@ def part2(data):
     groups = [x for x in string.split('3') if x != '']
     print(groups)
     # Each "run" produces branches
-    branches = [2 * (len(x) - 1) or 1 for x in groups]
+    branches = [calc_perms(len(x)) for x in groups]
     # Branches multiplied out to get solution
     product = 1
     for n in branches:
